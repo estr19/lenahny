adaa7241f76804076ce2d7d4791941c67dd7cd1f
Elena Strakhova <elenastrakhova@gmail.com>
2023-02-20 22:06:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {useEffect} from 'react';
 
 function App() {
+  let showDate = new Date("January 1, 2024 00:00");
+  const [showTime, setShowTime] = useState([]);
+  const [showDiscussion, setShowDiscussion] = useState(false);
+  
+  const meetingCountdown = () => {
+    let newObjects = [];
+    const today = new Date();
+    const difference = showDate - today;
+    
+    let displayDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let displayHours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+    let displayMinutes = Math.floor((difference / 1000 / 60) % 60);
+    let displaySeconds = Math.floor((difference / 1000) % 60);
+    
+    if (displayDays < 10) displayDays = "0" + displayDays;
+    if (displayHours < 10) displayHours = "0" + displayHours;
+    if (displayMinutes < 10) displayMinutes = "0" + displayMinutes;
+    if (displaySeconds < 10) displaySeconds = "0" + displaySeconds;
+
+    if (difference > 0) {
+      newObjects = {
+        days: displayDays,
+        hours: displayHours,
+        minutes: displayMinutes,
+        seconds: displaySeconds,
+      }
+    } else {
+      setShowDiscussion(true);
+    }
+    return setShowTime(newObjects);
+  }
+
+  useEffect(() => {
+    const tick = setTimeout(() => {
+      meetingCountdown();
+    }, 1000);
+    return () => clearInterval(tick);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex row justify-contentcenter align-content-center align-items-center vh-100 text-center">
+      <h1 className="display-5 fw-bold m-3"><span className='opaque'>It's the final countdown!</span></h1>
+      <div className='d-flex p-4 m-3 flex-row flex-wrap justify-content-evenly align-items-center'>
+        <h3 id="nextMtg" className='fw-bold'><span className='opaque'>{showDiscussion ? 'Discussing the book at the moment 😁' : <span id='mtgString'>{showTime.days} days, {showTime.hours} hours, {showTime.minutes} minutes, <span className='pulse'>{showTime.seconds}</span> seconds</span>}</span></h3>
+      </div>
     </div>
   );
 }
